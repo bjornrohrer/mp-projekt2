2da6bb862eee224e9290934c32ba7524833a46c9
bjornrohrer <113996059+bjornrohrer@users.noreply.github.com>
2026-04-02 18:57:53 +0200

    Added comments for the outline of linked list

---
+++ src/list.c
@@ -0,0 +1,26 @@
+/* Create node */
+
+/* Push node to head */
+
+/* Push node to tail */
+
+/* Pop node from head */
+
+/* Pop node from tail */
+
+/* Look at head without removing it */
+
+/* Look at tail without removing it */
+
+/* Get number of nodes in list */
+
+/* Get node at specific index */
+
+/* Find node by number and suit of card */
+
+/* Split list by specific node returning the tail */
+
+/* Append an entire sublist to the tail of a list */
+
+/* Free all nodes in a list */
+
