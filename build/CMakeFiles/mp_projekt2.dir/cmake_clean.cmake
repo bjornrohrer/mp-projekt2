35924740892823a5ce30bb0f26216bc317970768
Bjørn Arthur Rohrer <bar@betternow.org>
2026-04-30 22:36:53 +0200

    Fix: Added build files

---
+++ build/CMakeFiles/mp_projekt2.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mp_projekt2.dir/main.c.o"
+  "CMakeFiles/mp_projekt2.dir/main.c.o.d"
+  "CMakeFiles/mp_projekt2.dir/src/card.c.o"
+  "CMakeFiles/mp_projekt2.dir/src/card.c.o.d"
+  "CMakeFiles/mp_projekt2.dir/src/deck.c.o"
+  "CMakeFiles/mp_projekt2.dir/src/deck.c.o.d"
+  "CMakeFiles/mp_projekt2.dir/src/game_logic.c.o"
+  "CMakeFiles/mp_projekt2.dir/src/game_logic.c.o.d"
+  "CMakeFiles/mp_projekt2.dir/src/input.c.o"
+  "CMakeFiles/mp_projekt2.dir/src/input.c.o.d"
+  "CMakeFiles/mp_projekt2.dir/src/list.c.o"
+  "CMakeFiles/mp_projekt2.dir/src/list.c.o.d"
+  "CMakeFiles/mp_projekt2.dir/src/main_text.c.o"
+  "CMakeFiles/mp_projekt2.dir/src/main_text.c.o.d"
+  "CMakeFiles/mp_projekt2.dir/src/text_ui.c.o"
+  "CMakeFiles/mp_projekt2.dir/src/text_ui.c.o.d"
+  "mp_projekt2"
+  "mp_projekt2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mp_projekt2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/mp_projekt2.dir/src/input.c.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/mp_projekt2.dir/src/input.c.o: \
+  /Users/bjornarthurrohrer/c/mp-projekt2/src/input.c
